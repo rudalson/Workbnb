93c3b577c26e7136de59edd555432071d5b444cb
rudalson <rudalson@gmail.com>
2022-03-06 23:10:38 +0900

    added : dummy data로 출력한 post component

--- App.js
+++ App.js
@@ -13,6 +13,9 @@ import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 import HomeScreen from './src/screens/Home';
 import Post from './src/components/Post';
+import feed from './assets/data/feed';
+
+const post1 = feed[0];
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -25,9 +28,8 @@ const App = () => {
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       {/* <HomeScreen /> */}
-      <Post />
-      <Post />
-      <Post />
+      <Post post={post1} />
+      <Post post={feed[1]} />
     </SafeAreaView>
   );
 };
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -3,27 +3,31 @@ import {View, Text, Image} from 'react-native';
 import styles from './styles';
 
 const Post = props => {
+  const post = props.post;
+  console.log(post);
+
   return (
     <View style={styles.container}>
       <Image
         style={styles.image}
         source={{
-          uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyA8CvOs7xWhakFRoIYpK0OZ-ybhZ3joVQjg&usqp=CAU',
+          uri: post.image,
         }}
       />
-      <Text style={styles.office}>100인 이하 사무실</Text>
-      <Text style={styles.description} numberOfLines={2}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book.
-      </Text>
-      <Text style={styles.prices}>
-        <Text style={styles.oldPrice}>₩100</Text>
-        <Text style={styles.price}> ₩120</Text> / 월
+      <Text style={styles.office}>
+        {post.bed} bed {post.bedroom} bedroom
       </Text>
 
-      <Text style={styles.totalPrice}>총 800</Text>
+      <Text style={styles.description} numberOfLines={2}>
+        {post.type}. {post.title}
+      </Text>
+
+      <Text style={styles.prices}>
+        <Text style={styles.oldPrice}>₩{post.oldPrice}</Text>
+        <Text style={styles.price}> ₩{post.newPrice}</Text> / 월
+      </Text>
+
+      <Text style={styles.totalPrice}>Total ₩{post.totalPrice}</Text>
     </View>
   );
 };
