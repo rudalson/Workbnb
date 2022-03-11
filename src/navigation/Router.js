4c3f9cee2abeb760d74c557bd80d289df79b590e
rudalson <rudalson@gmail.com>
2022-03-11 19:44:24 +0900

    added : bottom navigation 추가

--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -5,6 +5,8 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import DestinationSearchScreen from '../screens/DestinationSearch';
 import GuestsScreen from '../screens/Guests';
 
+import HomeTabNavigator from './HomeTabNavigator';
+
 const Stack = createNativeStackNavigator();
 
 const Router = props => {
@@ -12,14 +14,14 @@ const Router = props => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="Destination Search"
-          component={DestinationSearchScreen}
-          options={{title: '원하는 지역을 검색하세요'}}
+          name="Home"
+          component={HomeTabNavigator}
+          options={{headerShown: false}}
         />
         <Stack.Screen
-          name="Guests"
-          component={GuestsScreen}
-          options={{title: '인원수 선택'}}
+          name="Destination Search"
+          component={DestinationSearchScreen}
+          options={{title: '목적지를 정하세요'}}
         />
       </Stack.Navigator>
     </NavigationContainer>
