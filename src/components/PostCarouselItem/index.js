0a85b9d328f6b89f4da80a297597ed71e8efc456
rudalson <rudalson@gmail.com>
2022-03-27 20:00:11 +0900

    added : carousel 컴포넌트 추가

---
+++ src/components/PostCarouselItem/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {View, Text, Image, useWindowDimensions} from 'react-native';
+import styles from './styles';
+
+const PostCarouselItem = props => {
+  const post = props.post;
+  const width = useWindowDimensions().width;
+
+  return (
+    <View style={[styles.container, {width: width - 60}]}>
+      <View style={styles.innerContainer}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: post.image,
+          }}
+        />
+
+        <View style={{flex: 1, marginHorizontal: 10}}>
+          <Text style={styles.office}>
+            {post.bed} bed {post.bedroom} bedroom
+          </Text>
+
+          <Text style={styles.description} numberOfLines={2}>
+            {post.type}. {post.title}
+          </Text>
+
+          <Text style={styles.prices}>
+            <Text style={styles.price}> ₩{post.newPrice}</Text> / 월
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default PostCarouselItem;
--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 import CustomMarker from '../../components/CustomMarker';
 import places from '../../../assets/data/feed';
+import PostCarouselItem from '../../components/PostCarouselItem';
 
 const SearchResultsMap = props => {
   const [selectedPlaceId, setSelectedPlaceId] = React.useState(null);
@@ -11,7 +12,7 @@ const SearchResultsMap = props => {
     <View>
       <MapView
         style={{width: '100%', height: '100%'}}
-        provider={PROVIDER_GOOGLE}
+        // provider={PROVIDER_GOOGLE}
         initialRegion={{
           latitude: 37.50922,
           longitude: 127.04869,
@@ -27,6 +28,10 @@ const SearchResultsMap = props => {
           />
         ))}
       </MapView>
+
+      <View style={{position: 'absolute', bottom: 40}}>
+        <PostCarouselItem post={places[0]} />
+      </View>
     </View>
   );
 };
